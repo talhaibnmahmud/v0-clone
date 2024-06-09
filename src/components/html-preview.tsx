36ed786cff41a9c7b721086e446f34b26caa8378
Talha Ibne Mahmud <71333752+talhaibnmahmud@users.noreply.github.com>
2024-06-10 00:38:35 +0600

    ✅ Done: State update working

--- src/components/html-preview.tsx
+++ src/components/html-preview.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import { useEffect, useRef } from "react";
+import ContentEditable from "react-contenteditable";
 
 import useHtmlStore from "@/stores/html";
 
 export function HtmlPreview({ htmlContent }: { htmlContent: TrustedHTML }) {
   const { html, setHtml } = useHtmlStore();
+  const textRef = useRef<string>("");
+
   const iframeRef = useRef<HTMLIFrameElement>(null);
 
   useEffect(() => {
@@ -37,19 +40,39 @@ export function HtmlPreview({ htmlContent }: { htmlContent: TrustedHTML }) {
     setHtml(newContent);
   };
 
+  const handleChange = (evt: React.FormEvent<HTMLDivElement>) => {
+    console.log(evt.currentTarget.innerHTML);
+    textRef.current = evt.currentTarget.innerHTML;
+  };
+
+  const handleBlur = () => {
+    console.log(textRef.current);
+    setHtml(textRef.current);
+  };
+
   if (!htmlContent) return null;
 
   return (
-    <iframe
-      ref={iframeRef}
-      onLoad={() => {
-        const iframeDoc = iframeRef.current?.contentDocument;
-        if (iframeDoc) {
-          iframeDoc.designMode = "on"; // Enable content editing inside the iframe
-          iframeDoc.addEventListener("input", onInput);
-        }
-      }}
-      style={{ width: "100%", height: "500px", border: "none" }}
-    />
+    <div>
+      <ContentEditable
+        onChange={handleChange}
+        onBlur={handleBlur}
+        html={html}
+      />
+    </div>
   );
+
+  // return (
+  //   <iframe
+  //     ref={iframeRef}
+  //     onLoad={() => {
+  //       const iframeDoc = iframeRef.current?.contentDocument;
+  //       if (iframeDoc) {
+  //         iframeDoc.designMode = "on"; // Enable content editing inside the iframe
+  //         iframeDoc.addEventListener("input", onInput);
+  //       }
+  //     }}
+  //     style={{ width: "100%", height: "500px", border: "none" }}
+  //   />
+  // );
 }
