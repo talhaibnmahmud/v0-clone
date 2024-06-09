ed1d178986e942390c654245341ab2c8b3c5fd1e
Talha Ibne Mahmud <71333752+talhaibnmahmud@users.noreply.github.com>
2024-06-09 22:40:37 +0600

    🧼 Update: Made html content editable

--- src/components/html-preview.tsx
+++ src/components/html-preview.tsx
@@ -1,3 +1,24 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 export function HtmlPreview({ htmlContent }: { htmlContent: TrustedHTML }) {
-  return <div dangerouslySetInnerHTML={{ __html: htmlContent }} />;
+  const [html, setHtml] = useState(htmlContent);
+  useEffect(() => {
+    console.log(htmlContent);
+  }, [htmlContent]);
+
+  if (!htmlContent) return null;
+
+  const onInput = (e: React.FormEvent<HTMLDivElement>) => {
+    setHtml(e.currentTarget.innerHTML);
+  };
+
+  return (
+    <div
+      contentEditable
+      dangerouslySetInnerHTML={{ __html: html }}
+      onInput={onInput}
+    />
+  );
 }
